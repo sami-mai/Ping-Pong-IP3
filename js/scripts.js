5a47ea00074e7c58de80ebd6eeddf53ee027aa0b
sami-mai <samirah.maison@gmail>
2017-09-22 16:15:11 +0000

    add function exceptions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 // business logic
+var number = parseInt($("input#year").val());
 
+function count() {
+
+};
+
+function exceptions(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "pingpong";
+  } else if (number % 5 === 0) {
+    return "pong";
+  } else if (number % 3 === 0) {
+    return "ping";
+  } else {
+    return number;
+  }
+};
 
 
 
